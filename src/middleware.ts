ed06650af529dfc338ad5b62e28a4a1e16be9f19
Aleksa Radovic <aleksa.radovic@rechargeapps.com>
2023-09-04 19:21:00 +0200

    adding public routes to auth middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,8 @@
 import { authMiddleware } from '@clerk/nextjs/server';
 
-export default authMiddleware();
+export default authMiddleware({
+  publicRoutes: ['/', '/drinks', '/:locale/sign-in', '/:locale/sign-up', '/api/(.*)'],
+});
 
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
