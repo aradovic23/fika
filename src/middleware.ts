260565a0168ec7a4a966d0e320ead764bff4c7bd
Aleksa Radovic <aleksa.radovic@rechargeapps.com>
2023-09-04 17:29:39 +0200

    middleware change

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,6 @@
 import { authMiddleware } from '@clerk/nextjs/server';
 
-export default authMiddleware({
-  publicRoutes: ['/', '/drinks'],
-});
+export default authMiddleware();
 
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
