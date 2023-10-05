b82c10f1bbc1f740330aeac0045d9670371a1f73
aradovic23 <radovic.aleksa@gmail.com>
2023-10-05 18:38:48 +0200

    change no results icon

--- src/components/NoResults.tsx
+++ src/components/NoResults.tsx
@@ -1,11 +1,11 @@
 import { Heading, Stack, Text } from '@chakra-ui/react';
-import { AlertTriangle } from 'lucide-react';
+import { AlertTriangle, Ghost } from 'lucide-react';
 
 export const NoResults = () => {
   return (
     <Stack alignItems="center" mt="5">
-      <AlertTriangle size={32} />
-      <Heading size="lg">No results found 🥺</Heading>
+      <Ghost size={32} />
+      <Heading size="lg">No results found </Heading>
       <Stack alignItems="center">
         <Text>We couldn&apos;t find what you searched for.</Text>
         <Text>Try searching again.</Text>
